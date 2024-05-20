aeae7346c07e57837be62894b6487ac6f42d84e6
Pablo <ezekielgonzalezr@gmail.com>
2024-05-19 21:36:54 -0600

    refactor: Update Footer component styling and logo size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import Socials from "./Socials";
 
 const Footer = () => {
   return (
-    <div className="bg-brown pt-2 px-2 lg:px-10 font-roboto">
-      <div className="flex justify-between py-5 lg:py-3 lg:px-5">
+    <div className="bg-brown pt-5 px-2 lg:px-10 font-roboto">
+      <div className="flex justify-between py-5 lg:py-3 px-5">
         <Socials />
         <div className="hidden lg:block">
-          <img src="/logo.png" alt="logo" className="w-28" />
+          <img src="/logo.png" alt="logo" className="w-24" />
         </div>
       </div>
       <p className="text-white text-lg text-center border-t border-white py-1 font-italiana">
