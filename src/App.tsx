fd0865bf8d24587294762d77069ac52c819afbc3
Pablo <ezekielgonzalezr@gmail.com>
2024-05-20 22:18:15 -0600

    feat: Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const BrowserRouter = createBrowserRouter([{ path: "/", element: <Home /> }]);
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <RouterProvider router={BrowserRouter} />
       <Footer />
     </>
   );
